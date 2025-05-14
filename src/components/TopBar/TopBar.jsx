2f6a727292cc8bd44155d7c44a5901665100d603
MathDEV-0 <teteuviegas.2006@hotmail.com>
2025-05-14 11:10:53 -0300

    No processo de estilização do Feed, criando componente TopBar que será bem reutilizável

---
+++ src/components/TopBar/TopBar.jsx
@@ -0,0 +1,46 @@
+// src/components/TopBar/TopBar.js
+
+import React from "react";
+import styles from "./TopBar.module.css";
+
+const TopBar = () => {
+  return (
+    <div className={styles.topBar}>
+      {/* Itens de texto */}
+      <span className={styles.item}>Perfil</span>
+      <div className={styles.separator}></div> {/* Traço entre itens */}
+      <span className={styles.item}>Comunidades</span>
+      <div className={styles.separator}></div> {/* Traço entre itens */}
+      <span className={styles.item}>Sinos Flix</span>
+      <div className={styles.separator}></div> {/* Traço entre itens */}
+      <span className={styles.item}>Repositórios</span>
+      <div className={styles.separator}></div> {/* Traço entre itens */}
+      {/* Ícones */}
+      <img
+        src="/assets/icons/cart.png"
+        alt="Carrinho"
+        className={styles.icon}
+      />
+      <div className={styles.separator}></div> {/* Traço entre itens */}
+      <img
+        src="/assets/icons/search.png"
+        alt="Buscar"
+        className={styles.icon}
+      />
+      <div className={styles.separator}></div> {/* Traço entre itens */}
+      <img
+        src="/assets/icons/bell.png"
+        alt="Notificações"
+        className={styles.icon}
+      />
+      <div className={styles.separator}></div> {/* Traço entre itens */}
+      <img
+        src="/assets/icons/settings.png"
+        alt="Configurações"
+        className={styles.icon}
+      />
+    </div>
+  );
+};
+
+export default TopBar;
--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import Retangulo from "../../components/Retangulo/Retangulo";
+import { Link } from "react-router-dom";
+import TopBar from "../../components/TopBar/TopBar";
+import styles from "./Feed.module.css";
 
 const Feed = () => {
   return (
     <div>
-      <h1>Bem-vindo ao feed!</h1>
+      <Retangulo tipo="1">
+        <img src="/assets/logo.png" alt="Logo" className={styles.logoImage} />
+      </Retangulo>
+      <Retangulo tipo="12">
+        <TopBar />
+      </Retangulo>
     </div>
   );
 };
